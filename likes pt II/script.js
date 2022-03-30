6ddfaff605eecc23c4d63ceab38122e3dd11786a
Andrew Castellanos <99152789+AndrewCastellanos@users.noreply.github.com>
2022-03-29 19:04:04 -0700

    Add files via upload

---
+++ likes pt II/script.js
@@ -0,0 +1,24 @@
+var like = 9;
+function clickCounter1(){
+    like++
+    console.log(like);
+    document.getElementById("likescount1").innerText = like + " like(s)"
+    
+}
+
+var like = 12;
+function clickCounter2(){
+    like++
+    console.log(like);
+    document.getElementById("likescount2").innerText = like + " like(s)"
+    
+}
+var like = 9;
+function clickCounter3(){
+    like++
+    console.log(like);
+    document.getElementById("likescount3").innerText = like + " like(s)"
+    
+}
+
+
---
+++ likes/script.js
@@ -0,0 +1,7 @@
+var like = 3;
+function clickCounter(){
+    like++
+    console.log(like);
+    document.getElementById("likescount").innerText = like + " like(s)"
+    
+}
